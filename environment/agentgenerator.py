e692b190885d7b386275307205b898531b0f4af4
hughie <=>
2025-03-14 16:05:58 +0000

    should be generating agents now but still crashes

--- environment/agentgenerator.py
+++ environment/agentgenerator.py
@@ -1,27 +1,63 @@
 import critters
+import random
+
+random.seed()
 
 class AgentGenerator:
     
     def __init__(self, width, height):
+        
         self._width = width
         self._height = height
         
-        self.hunters = []
-        self.runners = []
-        self.obstacles = []
     
-    def populate(self, environmentModel):
-        # TODO actually make the damn critters!
+    def populate(self, environment_model):
+        hunters = []
+        runners = []
+        obstacles = []
         
-        for h in self.hunters:
-            environmentModel.hunters.append(h)
-            environmentModel.schedule.add(h)
-            environmentModel.grid.place_agent(h, (h.initial_x, h.initial_y))
-        for r in self.runners:
-            environmentModel.runners.append(r)
-            environmentModel.schedule.add(r)
-            environmentModel.grid.place_agent(r, (r.initial_x, r.initial_y))
-        for o in self.obstacles:
-            environmentModel.obstacles.append(o)
-            environmentModel.schedule.add(o)
-            environmentModel.grid.place_agent(o, (o.initial_x, o.initial_y))
+        # TODO actually make the damn critters!
+        runner_count: int = int(self._width / 8)
+        hunter_count: int = int(runner_count / 2) - 1
+        obstacle_count: int = int(self._width * self._height * 0.5 * 0.1)
+        forest_band = (
+            int(self._height / 4), 
+            int(self._height / 4 * 3)
+        )
+        
+        # making runners
+        for i in range(runner_count):
+            x_position = random.randint(0, 7) + 8 * i
+            new_runner = critters.Runner(f"r{i}", environment_model, x_position, 0, random.randint(0, 8))
+            runners.append(new_runner)
+            
+        # making hunters
+        for i in range(hunter_count):
+            x_position = random.randint(0, 15) + 16 * i
+            y_position = random.randint(*forest_band)
+            new_hunter = critters.Hunter(f"h{i}", environment_model, x_position, y_position)
+            hunters.append(new_hunter)
+            
+        # making obstacles
+        for i in range(obstacle_count):
+            # find a space
+            x_position = random.randint(0, self._width)
+            y_position = random.randint(*forest_band)
+            while (x_position, y_position) not in environment_model.grid.empties: #! eww...
+                x_position = random.randint(0, self._width)
+                y_position = random.randint(*forest_band)
+            new_obstacle = critters.Obstacle(f"o{i}", environment_model, x_position, y_position)
+            
+        
+        for h in hunters:
+            environment_model.hunters.append(h)
+            environment_model.schedule.add(h)
+            environment_model.grid.place_agent(h, (h.initial_x, h.initial_y))
+        for r in runners:
+            environment_model.runners.append(r)
+            environment_model.schedule.add(r)
+            environment_model.grid.place_agent(r, (r.initial_x, r.initial_y))
+        for o in obstacles:
+            environment_model.obstacles.append(o)
+            environment_model.schedule.add(o)
+            environment_model.grid.place_agent(o, (o.initial_x, o.initial_y))
